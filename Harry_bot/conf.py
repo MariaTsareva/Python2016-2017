427d056311c2c1b970c41ebe67c777a9ad4b63b5
MariaTsareva <ladymary9627@gmail.com>
2017-06-20 13:48:19 +0300

    Brand new bot. Final version of project bot

---
+++ Harry_bot/conf.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  2 15:46:13 2017
+
+@author: stsaryov
+"""
+
+TOKEN = '390466435:AAEnFSNq2Jxq5pqZHV90Q_duX5-1xEKcs8Q'
+WEBHOOK_HOST = 'MaryTsar.pythonanywhere.com'
+WEBHOOK_PORT = '443'
---
+++ Harry_bot/phrases.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 16 20:17:08 2017
+
+@author: stsaryov
+"""
+
+english = ['Well, you can send me something in English, but I will use only Russian anyway. Sorry not sorry',\
+           'Do not speak English, verstehen?', 'Free-latin zone']
+help = 'Нужно ввести любое предложение на русском языке и дождаться ответа от бота.'
+start = "Привет! Введи сообщение, и бот составит для тебя фразу из 'Гарри Поттера'."
